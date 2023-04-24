de51f4de898c021c5b3af5e27cec932e260ebe55
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-04-24 12:43:47 +0300

    get product and order by id

--- order-fastapi/OrdersApp/main.py
+++ order-fastapi/OrdersApp/main.py
@@ -1,8 +1,8 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+import models
 from models import Orders, Products
 from database import engine, SessionLocal
 
@@ -19,11 +19,27 @@ def get_db():
         db.close()
 
 
-@app.get('/orders/')
-def read_all_orders(db: Annotated[Session, Depends(get_db)]):
+@app.get('/orders')
+async def read_all_orders(db: Annotated[Session, Depends(get_db)]):
     return db.query(Orders).all()
 
 
 @app.get('/products/')
-def read_all_products(db: Annotated[Session, Depends(get_db)]):
+async def read_all_products(db: Annotated[Session, Depends(get_db)]):
     return db.query(Products).all()
+
+
+@app.get('/orders/{order_id}')
+async def get_order_by_id(db: Annotated[Session, Depends(get_db)], order_id: int):
+    order_model = db.query(Orders).filter(Orders.id == order_id).first()
+    if order_model is not None:
+        return order_model
+    raise HTTPException(status_code=404, detail="Order not found!")
+
+
+@app.get('/products/{product_id}')
+async def get_product_by_id(db: Annotated[Session, Depends(get_db)], product_id: int):
+    product_model = db.query(Products).filter(Products.id == product_id).first()
+    if product_model is not None:
+        return product_model
+    raise HTTPException(status_code=404, detail="Product not found!")
