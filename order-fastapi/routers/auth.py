70c1bbfb7869856f8b5c14fce2df05253241d207
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-04-25 20:12:12 +0300

    create logic for auth user creation

--- order-fastapi/routers/auth.py
+++ order-fastapi/routers/auth.py
@@ -1,7 +1,44 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import SessionLocal
+from models import Users
+from passlib.context import CryptContext
 
 routers = APIRouter()
 
-@routers.get('/auth/')
-async def get_user():
-    return {'user': 'Authenticated'}
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+class CreateUserRequest(BaseModel):
+    email: str
+    username: str
+    first_name: str
+    last_name: str
+    password: str
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@routers.post('/auth', status_code=status.HTTP_201_CREATED)
+async def create_user(create_user_request: CreateUserRequest, db: Annotated[Session, Depends(get_db)]):
+    create_user_request = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        password=bcrypt_context.hash(create_user_request.password),
+    )
+
+    db.add(create_user_request)
+    db.commit()
